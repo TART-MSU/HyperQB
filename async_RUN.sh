cd27b4021be4f4e128621d6e0d80a5b91c5df714
tzuhan <haa831101@gmail.com>
2022-04-24 20:37:25 -0400

    concurrent leak

--- async_RUN.sh
+++ async_RUN.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 #coding:utf-8
-
 HYPERQUBE=./hyperqube.sh
 GENMODEL=./async_genmodel.sh
 SOLVEQBF=./async_solveqbf.sh
@@ -47,45 +46,36 @@ BUILD_V2=cases_async/buildtraj_v2.py # AAAE
 # TRAJ=12
 # MODEL1=cases_async/concleaks/original/conc_leaks_2procs.smv
 # MODEL2=cases_async/concleaks/original/conc_leaks_2procs.smv
-# BUILDTRAJ=cases_async/concleaks/original/buildtraj_v2.py
+# BUILDTRAJ=${BUILD_V1}
+# time ${GENMODEL} ${MODEL1} ${MODEL2} ${DUMMYPROP} ${M} hpes -find -multi
+# time python3 ${BUILDTRAJ} ${M} ${M} ${TRAJ}
+# time ${SOLVEQBF}
+
+### uncomment to run conc_leak new
+# M=6
+# TRAJ=12
+# MODEL1=cases_async/concleaks/with_ndet/conc_leaks_3procs.smv
+# MODEL2=cases_async/concleaks/with_ndet/conc_leaks_3procs.smv
+# BUILDTRAJ=${BUILD_V2}
 # time ${GENMODEL} ${MODEL1} ${MODEL2} ${DUMMYPROP} ${M} hpes -find -multi
 # time python3 ${BUILDTRAJ} ${M} ${M} ${TRAJ}
 # time ${SOLVEQBF}
 
 
-# Example 3
-# For checking and debugging
-# python3 traj_parser.py
+########################
+#   optimiazation      #
+########################
 
 ### generate model, build trajectories, then solve QBF
-### uncomment to run example 1
-# M=8
-# TRAJ=16
-# MODEL1=cases_async/concleaks_new/conc_leaks_3procs.smv
-# MODEL2=cases_async/concleaks_new/conc_leaks_3procs.smv
-# BUILDTRAJ=cases_async/concleaks_new/build_conc_leaks_2trajs.py
-# time ${GENMODEL} ${MODEL1} ${MODEL2} ${DUMMYPROP} ${M} hpes -find -multi
-# time python3 ${BUILDTRAJ} ${M} ${M} ${M} ${M} ${TRAJ}
+### uncomment to run example original optimization
+M=8
+TRAJ=16
+MODEL1=cases_async/optimization/opt_LP_source.smv
+MODEL2=cases_async/optimization/opt_LP_target.smv
+BUILDTRAJ=cases_async/optimization/build_opt_1traj.py
+time ${GENMODEL} ${MODEL1} ${MODEL2} ${DUMMYPROP} ${M} hpes -find -multi
+time python3 ${BUILDTRAJ} ${M} ${M} ${TRAJ}
 # time ${SOLVEQBF}
-# ## parse the trajectory counterexample
-# python3 traj_parser.py
-
-
-
-# Optimization
-# For checking and debugging
-# python3 traj_parser.py
-
-# ### generate model, build trajectories, then solve QBF
-# ### uncomment to run example 1
-# M=8
-# TRAJ=16
-# MODEL1=cases_async/optimization/opt_LP_source.smv
-# MODEL2=cases_async/optimization/opt_LP_target.smv
-# BUILDTRAJ=cases_async/optimization/build_opt_1traj.py
-# time ${GENMODEL} ${MODEL1} ${MODEL2} ${DUMMYPROP} ${M} hpes -find -multi
-# time python3 ${BUILDTRAJ} ${M} ${M} ${TRAJ}
-# # time ${SOLVEQBF}
 
 # ### generate model, build trajectories, then solve QBF
 # ### uncomment to run DBE Original
