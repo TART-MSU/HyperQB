647273ee8d84b20648dd10a42553c43d4fbb3375
Pavel Shevchenko <pavelshevchenko@Pavels-MacBook-Pro.local>
2023-07-11 01:12:51 -0400

    "update"

--- ramdisk_tests.sh
+++ ramdisk_tests.sh
@@ -1,44 +1,44 @@
 
 
-# x=1
-# y=1
-# z=""
-# while [ $y -le 10000 ]
-# do
-#     while [ $x -le 10000 ]
-#     do
-#         echo $x >> test.txt
-#         ((x++))
-#     done
-#     while read -r line; 
-#     do
-#         z=${line}
-#     done < "test.txt"
-#     ((y++))
-# done
-
-# > test.txt
-
-
-
-time diskutil erasevolume apfs "RAMDisk" `hdiutil attach -nomount ram://100000`
-touch /Volumes/RAMDisk/test.txt
-
 x=1
 y=1
 z=""
-while [ $y -le 10000 ]
+while [ $y -le 1000 ]
 do
     while [ $x -le 10000 ]
     do
-        echo $x >> /Volumes/RAMDisk/test.txt
+        echo $x >> test.txt
         ((x++))
     done
     while read -r line; 
     do
         z=${line}
-    done < "/Volumes/RAMDisk/test.txt"
+    done < "test.txt"
     ((y++))
 done
 
-time diskutil unmount /Volumes/RAMDisk
+> test.txt
+
+
+
+# time diskutil erasevolume apfs "RAMDisk" `hdiutil attach -nomount ram://10000000`
+# touch /Volumes/RAMDisk/test.txt
+
+# x=1
+# y=1
+# z=""
+# while [ $y -le 1000 ]
+# do
+#     while [ $x -le 10000 ]
+#     do
+#         echo $x >> /Volumes/RAMDisk/test.txt
+#         ((x++))
+#     done
+#     while read -r line; 
+#     do
+#         z=${line}
+#     done < "/Volumes/RAMDisk/test.txt"
+#     ((y++))
+# done
+
+# diskutil unmount /Volumes/RAMDisk
