59a3514acee414a74a65f77e9a00c4062cdf5473
tzuhan <haa831101@gmail.com>
2023-11-01 08:36:44 +0100

    update shell scripts

---
+++ run_compile.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+######################################
+# compile all necessary executable   #
+######################################
+
+### new genqbf with partial multi-gate ###
+# cd src/expression/
+# make clean
+# make 
+# cp bin/genqbf ../../exec/genqbf_partialmulti
+# make clean
+# cd ../..
+
+### genqbf with binary gate ###
+# cd src/genqbf/
+# make clean
+# make
+# cp bin/genqbf ../../exec/genqbf_bingate
+# make clean
+# cd ../..
+
+### genqbf with specpfic cases ###
+# cd src/genqbfv5/
+# make clean
+# make
+# cp bin/genqbf ../../exec/genqbf_v5
+# make clean
+# cd ../..
+
+cd src/parser
+g++ -std=c++17 -Wall -o parser parser.cpp
+# g++ parser.cpp -o parser 
+cp parser ../../exec/parser 
+cd ../..
