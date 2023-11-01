26d00dec5406c280fb2e8ccd948b0d51ff8ef003
tzuhan <haa831101@gmail.com>
2023-11-01 08:36:44 +0100

    update shell scripts

--- run_demos.sh
+++ run_demos.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
-#####################################################################################################
-#  This script runs ALL experiments (0.1-11.1) we provided in Table 3 of our tool paper submission  #
-#####################################################################################################
+############################
+#  Simple demos of HyperQB #
+############################
+HYPERQB="./hyperqb.sh"
 
-### Requirements:
-###   1. Before running, please make sure Docker is installed: (https://docs.docker.com/get-docker/)
-###   2. Apple Chop Macs might need to use: --platform linux/amd64
+### [demo 0: run simple_krip with simple_krip_formula]
+${HYPERQB} demo/mini.smv demo/mini.smv demo/mini.hq 3 -pes -bughunt
+${HYPERQB} demo/mini2.smv demo/mini2.smv demo/mini2.hq 3 -pes -find
 
-### DEMOS, see README for detailed explination
+### [demo 1: run info-flow example]
+${HYPERQB} demo/infoflow.smv demo/infoflow.smv demo/infoflow1.hq 5 -pes -debug
+# ${HYPERQB} demo/infoflow.smv demo/infoflow.smv demo/infoflow.smv demo/infoflow2.hq 5 -pes -debug
 
-### [demo 1: run bakery algorithm with symmetry property]
-# ./hyperqb.sh demo/bakery.smv demo/bakery.smv demo/symmetry.hq 10 -pes
-### [dem0 2: run SNARK algorithm with linearizability propoerty]
-# ./hyperqb.sh demo/SNARK_conc.smv demo/SNARK_seq.smv demo/lin.hq 18 -pes
-### [demo 3: run simple_krip with simple_krip_formula]
-# ./hyperqb.sh demo/mini.smv demo/mini.smv demo/mini.hq 3 -pes
-# ./hyperqb.sh demo/mini.smv demo/mini.smv demo/mini.smv demo/mini.smv  demo/mini_try.hq 10 -pes -find
-# ./hyperqb.sh demo/mini.smv demo/mini.smv demo/mini.smv demo/mini.hq 3 -pes
+### [demo 2: run bakery algorithm with symmetry property]
+# ${HYPERQB} demo/bakery.smv demo/bakery.smv demo/symmetry.hq 10 -pes -debug
+
+### [demo 3: run SNARK algorithm with linearizability propoerty, bug1]
+# ${HYPERQB} demo/snark1_conc.smv demo/snark1_seq.smv demo/lin.hq 18 -pes -debug
+
+
+
+
+
+
+
+
---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "HyperQB setup starts"
+
+sudo apt update
+
+echo "[ setup docker and a helper image... ]"
+sudo snap install docker
+sudo docker pull tzuhanmsu/hyperqube:latest
+
+echo "[ setpup dotnet for comparison with AH and AHQ... ]"
+sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-7.0
+sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-7.0
+sudo apt-get install -y dotnet-runtime-7.0
+
+echo "check docker: "
+docker --version
+
+echo "check dotnet: "
+dotnet --version
+
+
+echo "HyperQB setup success!"
